0f8cda6bb66fe70db604096d0c9806723e55421e
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-04-15 14:21:40 +0000

    mensagem

---
+++ Aula_02/ex01.py
@@ -0,0 +1,4 @@
+print("Digite seu nome completo:")
+nome_completo = input()
+primeiro_nome = nome_completo.split()
+print(f"Bem-vindo(a) ao Python, {primeiro_nome[0]}")
---
+++ Aula_02/ex02.py
@@ -0,0 +1,6 @@
+print("Digite a nota do primeiro bimestre da disciplina:")
+primeiro_bimestre = int(input())
+print("Digite a nota do segundo bimestre da disciplina:")
+segundo_bimestre = int(input())
+média = (primeiro_bimestre + segundo_bimestre) // 2
+print(f"Média parcial = {média}")
