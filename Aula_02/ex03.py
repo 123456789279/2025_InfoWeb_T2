64c8056d27f81a40c65a53dc6bbc060b3bcdaaf3
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-04-15 14:46:47 +0000

    mensagem

--- Aula_02/ex03.py
+++ Aula_02/ex03.py
@@ -1,3 +1,4 @@
+import math
 print("Digite a base e a altura do retângulo")
 base = int(input())
 altura = int(input())
@@ -11,4 +12,4 @@ width = float(input())
 perimetro = 2 * (height + width)
 
 #Diagonal
-#diagonal = 
+diagonal = math.sqrt(height**2 + width**2)
