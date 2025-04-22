81f0a89bd0c49a3f2d62e23098e2d39cd767a9b5
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-04-22 14:52:56 +0000

    mensagem

--- Aula_03/lista03.py
+++ Aula_03/lista03.py
@@ -10,8 +10,10 @@
 #mda = (A + B) / 2
 #print(f"MEDIA = {mda:.5f}")
 
-#1011
-pi = 3,14159
-R = int(input())
-volume = (4 / 3,0) * pi * R**3
-print(f"VOLUME = {volume:.3f}")
+#1011 fazer depois
+#pi = 3,14159
+#R = int(input())
+#volume = (4 / 3,0) * pi * R**3
+#print(f"VOLUME = {volume:.3f}")
+
+#2416
