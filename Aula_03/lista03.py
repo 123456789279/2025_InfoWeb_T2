27a4cd375ea0affa3354589413c6a25c1394ff8e
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-04-22 14:46:27 +0000

    mensagem

---
+++ Aula_03/lista03.py
@@ -0,0 +1,17 @@
+#1004
+#val = int(input())
+#val2 = int(input())
+#prod = val * val2
+#print(f"PRODUÇÃO = {prod}")
+
+#1005
+#A = float(input())
+#B = float(input())
+#mda = (A + B) / 2
+#print(f"MEDIA = {mda:.5f}")
+
+#1011
+pi = 3,14159
+R = int(input())
+volume = (4,0/3) * pi * R
+print(f"VOLUME = {volume:.3f}")
