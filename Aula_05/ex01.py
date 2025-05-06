1bf4d8f41a81870ca9c8c267ed48707f106d3084
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-05-06 14:19:14 +0000

    mensagem

---
+++ Aula_05/ex01.py
@@ -0,0 +1,14 @@
+class circulo:
+    def _init_(self):
+        self.raio = 10
+    def area(self):
+        A = 3,14 * self.raio**2
+        return A
+    def circuferencia(self):
+        C = 2 * 3,14 * self.raio
+        return C
+
+x = circulo
+x.raio = 5
+print(x.area()) # o self e x
+print(x.circuferencia()) # o self e x
