70a3e0bf3e6398716b00efd1a9d3d4ef12529b21
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-05-06 14:34:36 +0000

    mensagem

--- Aula_05/ex02.py
+++ Aula_05/ex02.py
@@ -3,6 +3,10 @@ class viagem:
         self.distancia = 20
         self.tempo = 8.00
     def velocidade_media(self):
-        self.distancia / self.tempo
+        vm = self.distancia / self.tempo
+        return vm
 
 x = viagem()
+x.distancia = 25
+x.tempo = 8.30
+print(x.velocidade_media()) # o self e x
