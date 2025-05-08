827135082a510cbb3b3c96d9aee773d5412cebd4
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-05-08 13:14:09 +0000

    mensagem

--- Aula_06/ex_passados.py
+++ Aula_06/ex_passados.py
@@ -1,11 +1,11 @@
-#01
+# I
 #print(1 - 2 + 3 * 4) Saida: 11
 #print(1 * 2 - 3 * 4) Saida: -10
 #print(1 / 2 + 3 * 4) Saida: 12,5
 #print(1 // 2 * 3 + 4) Saida: 4
 #print(1 + 2 * 3 / 4) Saida: 2,5
 
-#02
+# II
 #print("Digite três números inteiros separados por espaços")
 #x, y, z = int(input()), int(input()), int(input())
 #if x % 2 == 0:
@@ -23,4 +23,12 @@
 #  if y % 2 != 0:
 #    print(f"Soma dos pares: {z + y}")
 
-#03  
+# III
+#print("Escreva uma frase")
+#frase = str(input())
+#fae = 0
+#while fae in frase(1, 21, 2):
+#    fae += frase
+#print(fae)
+
+# IV
