90fb5b2fa0ca46c0a0c969b20b7df78a3b24bae4
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-05-08 13:01:44 +0000

    mensagem

--- Aula_06/ex_passados.py
+++ Aula_06/ex_passados.py
@@ -6,3 +6,21 @@
 #print(1 + 2 * 3 / 4) Saida: 2,5
 
 #02
+#print("Digite três números inteiros separados por espaços")
+#x, y, z = int(input()), int(input()), int(input())
+#if x % 2 == 0:
+#  print(f"Soma dos pares: {x + y}")
+#  if x % 2 != 0:
+#    print(f"Soma dos pares: {x + z}")
+
+#if y % 2 == 0:
+#  print(f"Soma dos pares: {y + x}")
+#  if y % 2 != 0:
+#    print(f"Soma dos pares: {y + z}")
+
+#if z % 2 == 0:
+#  print(f"Soma dos pares: {z + x}")
+#  if y % 2 != 0:
+#    print(f"Soma dos pares: {z + y}")
+
+#03  
