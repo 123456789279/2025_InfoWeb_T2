77c7ad5ab80e833369b110e9f11180c34c1ff9bd
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-05-27 14:16:15 +0000

    mensagem

--- Aula_10/lista_poo.py
+++ Aula_10/lista_poo.py
@@ -1,42 +1,46 @@
 #1 - Circulo
-class circulo:
-    def _init_(self):
-        self.__raio = 10
-    def set_area(self):
-        A = 3,14 * self.__raio**2
-        return A
-    def get_circuferencia(self):
-        C = 2 * 3,14 * self.__raio
-        return C
+#class circulo:
+#    def _init_(self):
+#        self.__raio = 10
+#    def set_area(self):
+#        A = 3,14 * self.__raio**2
+#        return A
+#    def get_circuferencia(self):
+#        C = 2 * 3,14 * self.__raio
+#        return C
 
-x = circulo()
-x.__raio = 5
-print(x.set__area()) # o self e x
-print(x.get__circuferencia()) # o self e x    
+#x = circulo()
+#x.__raio = 5
+#print(x.set__area()) # o self e x
+#print(x.get__circuferencia()) # o self e x    
 
 #2 - Uma Viagem
-class viagem:
-    def _init_(self):
-        self.__distancia = 200
-        self.__tempo = 2
-    def set__velocidade_media(self):
-        vm = self.__distancia / self.__tempo
-        return vm
+#class viagem:
+#    def _init_(self):
+#        self.__distancia = 200
+#        self.__tempo = 2
+#    def set__velocidade_media(self):
+#        vm = self.__distancia / self.__tempo
+#        return vm
 
-x = viagem()
-x.distancia = 200
-x.tempo = 4
-print(x.set__velocidade_media()) # o self e x
+#x = viagem()
+#x.distancia = 200
+#x.tempo = 4
+#print(x.set__velocidade_media()) # o self e x
 
 #3 - Uma Conta Banacaria
 class conta_bancaria:
     def _init_(self):
         self.__titular = ""
         self.__numero = ""
-        self.__saldo = 100.000
+        self.__saldo = 0.0
     def set_titular(self, t):
         if t == "": raise ValueError()
         self.__titular = t
+    def get_titular(self):
+        return self.__titular
+    def get_saldo(self):
+        return self.__saldo
     def set__deposito(self):
         self.__saldo += self.__numero
         print(f"Depósito de {self.__valor} realizado com sucesso. Novo saldo: {self.__saldo}")
