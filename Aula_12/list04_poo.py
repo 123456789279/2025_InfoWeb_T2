92eb3f459f450c8de68e2d6ae88a6c785cbb995b
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-06-05 12:23:33 +0000

    mensagem

--- Aula_12/list04_poo.py
+++ Aula_12/list04_poo.py
@@ -8,9 +8,21 @@ class Viagem:
         if destino < 0: raise ValueError("O destino deve ser maior que 0")
         self.__destino = destino
     def set__distancia(self, distancia):
-        if distancia < 0: raise ValueError("A distancia deve ser maior que 0")
+        if distancia < 0: raise ValueError("A distancia percorrida deve ser maior que 0")
         self.__distancia = distancia
+    def set__litros(self, litros):
+        if litros < 0: raise ValueError("A quantidde de litros deve ser maior que 0")
+        self.__litros = litros
     def get__destino(self):
         return self.__destino
     def get__distancia(self):
         return self.__distancia
+    def get__litros(self):
+        return self.__litros
+    def consumo(self):
+        return self.__distancia // self.__litros
+
+class ViagemUI:
+    @staticmethod
+    def main():
+      x = Viagem()
