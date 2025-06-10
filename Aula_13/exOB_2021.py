7c7737b6e976837ed635853af74d31bd23c3e84d
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-06-10 14:30:43 +0000

    mensagem

--- Aula_13/exOB_2021.py
+++ Aula_13/exOB_2021.py
@@ -3,13 +3,13 @@
 Cibele = int(input())
 Camila = int(input())
 Celeste = int(input())
-if Cibele <= Camila and Camila <= Celeste:
-    print(Camila)
-else:
-    print(Celeste)
+x = [Cibele, Camila, Celeste]
+x.sort()
+print(x[1])
 
 #Zero para cancelar
 N = [int(input()), int(input()), int(input()), int(input()), int(input())]
+i = 0
 while i in N(1, 0):
-    if 1 ≤ N ≤ 100 000:
-        
+    if 1 <= N <= 100.000:
+        print()
