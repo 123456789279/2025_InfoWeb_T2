8abd02c5263561f47b75640bc0570ef85395cec5
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-06-10 14:20:43 +0000

    mensagemn

---
+++ Aula_13/exOB_2021I.py
@@ -0,0 +1,15 @@
+#Fase 1
+#Idade de Camila
+Cibele = int(input())
+Camila = int(input())
+Celeste = int(input())
+if Cibele <= Camila and Camila <= Celeste:
+    print(Camila)
+else:
+    print(Celeste)
+
+#Zero para cancelar
+N = [int(input()), int(input()), int(input()), int(input()), int(input())]
+while i in N(1, 0):
+    if 1 ≤ N ≤ 100 000:
+        
