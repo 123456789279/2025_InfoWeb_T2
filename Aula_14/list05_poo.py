3cf5cfc8bce057a4c23ae7b5cec8ba99008aca77
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-07-08 14:48:05 +0000

    mensagem

---
+++ Aula_14/list05_poo.py
@@ -0,0 +1,14 @@
+# 1. Um Paciente
+import datetime
+class Paciente:
+    def __init__(self):
+        self.__nome = ""
+        self.__cpf = ""
+        self.__telefone = ""
+        self.__nascimento = ""
+    def set__nome(self, nome):
+        self.__nome = nome
+    def set__cpf(self, cpf):
+        self.__cpf = cpf
+    def set__telefone(self, telefone):
+        self.__telefone = telefone
