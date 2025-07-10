ea9318e056a36425d106bdbd5ef9acefe9979353
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-07-10 12:15:58 +0000

    mensagem

--- Aula_14/list05_poo.py
+++ Aula_14/list05_poo.py
@@ -7,10 +7,14 @@ class Paciente:
         self.__telefone = ""
         self.__nascimento = ""
     def set__nome(self, nome):
+        nome = len(self.__nome)
+        if self.__nome <= 5: raise ValueError("O nome deve ter no minimo 5 letras")
         self.__nome = nome
     def set__cpf(self, cpf):
+        if self.__cpf <= 5: raise ValueError("O cpf deve ter no minimo 5 digitos")
         self.__cpf = cpf
     def set__telefone(self, telefone):
+        if self.__telefone <= 5: raise ValueError("O telefone deve ter no minimo 5 digitos")
         self.__telefone = telefone
     def set__nascimento(self, nascimento):
         self.__nascimento = nascimento
