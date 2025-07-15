7036555d65b407bfecde87b41c03e8672fd77434
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-07-15 14:03:09 +0000

    mensagem

--- Aula_15/listRevisao_poo.py
+++ Aula_15/listRevisao_poo.py
@@ -6,3 +6,19 @@ class Treino:
         self.__data = ""
         self.__distancia = ""
         self.__tempo = ""
+    def set__id(self, id):
+        if self.__id <= 5: raise ValueError("O id deve ter no minimo 5 digitos")
+        self.__id = id
+    def set__data(self, data):
+        self.__data = datetime.datetime.strptime(f"{int(input())}/{int(input())}/{int(input())}", "%d:%m:%y")
+        if self.__data <= 3: raise ValueError("A Data do Treino deve ter ano, mes e dia")
+        self.__data = data
+    def set__distancia(self, distancia):
+        self.__distancia = datetime.datetime.strptime(f"{int(input())}:{int(input())}:{int(input())}", "%H:%M:%S")
+        if self.__distancia <= 3: raise ValueError("A Distancia percorrida deve ter hora, minuto e segundo")
+        self.__data = distancia                
+
+    def get__id(self):
+        return self.__id
+    def get__data(self):
+        return self.__data
