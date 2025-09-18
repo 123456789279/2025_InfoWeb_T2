44ae4ac7bebd685e569c06299a594ade162ff551
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-18 13:06:04 +0000

    mensagem

--- Agenda2/servico.py
+++ Agenda2/servico.py
@@ -1,5 +1,17 @@
 class Servico:
-    def __init__(self, id, descricao, valor):
+    def __init__(self):
         self.__id = ""
         self.__descricao = ""
         self.__valor = ""
+    def set__id(self, id):
+        id = self.__id
+    def set__descricao(self, descricao):
+        descricao = self.__descricao
+    def set__valor(self, valor):
+        valor = self.__valor
+    def get__id(self):
+        return self.__id
+    def get__descricao(self):
+        return self.__descricao
+    def get__valor(self):
+        return self.__valor
