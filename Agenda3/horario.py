fcc51435419b46372480a63e0f3fd6f1f88c5db5
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-23 14:25:40 +0000

    mensagem

--- Agenda3/horario.py
+++ Agenda3/horario.py
@@ -33,7 +33,6 @@ class Horario:
        "confirmado":self.__confirmado, "id_cliente":self.__id_cliente,
        "id_servico":self.__id_servico}
      return dic
-   
    @staticmethod
    def from_json(dic):
      horario = Horario(dic["id"], datetime.strptime(dic["data"], "%d/%m/%Y %H:%M"))
@@ -41,3 +40,18 @@ class Horario:
      horario.set_id_cliente(dic["id_cliente"])
      horario.set_id_servico(dic["id_servico"])
      return horario
+   
+import json
+
+class HorarioDAO:
+  __objetos = []
+
+  @classmethod
+  def inserir(cls, obj):
+    cls.abrir()
+    id = 0
+    for aux in cls.__objetos:
+      if aux.get_id() > id: id = aux.get_id()
+    obj.set_id(id + 1)
+    cls.__objetos.append(obj)
+    cls.salvar()
