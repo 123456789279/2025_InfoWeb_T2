74ad38705b57f0ec09da2db1280c280b79ebb374
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-23 14:32:29 +0000

    mensagem

--- Agenda3/horario.py
+++ Agenda3/horario.py
@@ -78,3 +78,18 @@ class HorarioDAO:
     if aux != None:
       cls.__objetos.remove(aux)
       cls.salvar()
+  @classmethod
+  def abrir(cls):
+    cls.__objetos = []
+    try:
+      with open("horarios.json", mode ="r") as arquivo:
+        list_dic = json.load(arquivo)
+        for dic in list_dic:
+          obj = Horario.from_json(dic)
+          cls.__objetos.append(obj)
+    except FileNotFoundError:
+      pass
+  @classmethod
+  def salvar(cls):
+    with open("horarios.json", mode ="w") as arquivo:
+      json.dump(cls.__objetos, arquivo, default = Horario.to_json)
