034bceb0010b83b1008c2765084a2e1854ba9899
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-09-30 14:28:16 +0000

    mensagem

--- Projeto/index.py
+++ Projeto/index.py
@@ -1,15 +1,17 @@
 from templates.manterclienteUI import ManterClienteUI
 from templates.manterservicoUI import ManterServicoUI
 from templates.manterproficionalUI import ManterProficionalUI
+from templates.manterhorarioUI import ManterHorarioUI
 import streamlit as st
 
 class IndexUI:
 
     def menu_admin():            
-        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Serviços", "Cadastro de Proficionais"])
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Serviços", "Cadastro de Proficionais", "Cadastro de Horarios"])
         if op == "Cadastro de Clientes": ManterClienteUI.main()
         if op == "Cadastro de Serviços": ManterServicoUI.main()
         if op == "Cadastro de Proficionais": ManterProficionalUI.main()
+        if op == "Cadastro de Horarios": ManterHorarioUI.main()
 
     def sidebar():
         IndexUI.menu_admin()
