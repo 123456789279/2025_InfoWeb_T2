1746eae81b224199cedda5886467857aacca93f4
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-09 12:48:16 +0000

    mensagem

--- Projeto/views.py
+++ Projeto/views.py
@@ -65,3 +65,8 @@ class View:
         for c in View.cliente_listar():
            if c.get_email() == "admin": return
         View.cliente_inserir("admin", "admin", "fone", "1234")
+    def cliente_autenticar(email, senha):
+        for c in View.cliente_listar():
+           if c.get_email() == email and c.get_senha() == senha:
+              return {"id": c.get_id(), "nome": c.get_nome()}
+        return None
