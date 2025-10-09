6b94bafd818ccee3db10231584cf70837aef1572
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-09 13:23:26 +0000

    mensagem

--- Projeto/views.py
+++ Projeto/views.py
@@ -61,10 +61,6 @@ class View:
         horario = Proficional(id, "", "", "", "", "")
         HorarioDAO.excluir(horario) 
 
-    def cliente_criar_admin():
-        for c in View.cliente_listar():
-           if c.get_email() == "admin": return
-        View.cliente_inserir("admin", "admin", "fone", "1234")
     def cliente_autenticar(email, senha):
         for c in View.cliente_listar():
            if c.get_email() == email and c.get_senha() == senha:
