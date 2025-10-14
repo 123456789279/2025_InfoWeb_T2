f8b54d23e1792c08bb4019be03e2e67c8562f48e
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-14 14:27:35 +0000

    mensagem

--- Projeto/templates/abrirminhaagendaUI.py
+++ Projeto/templates/abrirminhaagendaUI.py
@@ -6,7 +6,7 @@ import datetime
 
 class AbrirMinhaAgendaUI:
       def main():
-          st.header("Abrir Conta no Sistema")
+          st.header("Abrir Agenda no Sistema")
           data = st.datetime_input("Informe a data no formato dd/mm/aaaa")
           horario_inicio = st.datetime_input("Informe o horario inicial no formato HH:MM")
           horario_final = st.datetime_input("Informe o horario final no formato HH:MM")
