9a89f68472dba1196e32f54bf2ef7327c6966058
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-16 12:34:24 +0000

    mensagem

--- Projeto/templates/abrirminhaagendaUI.py
+++ Projeto/templates/abrirminhaagendaUI.py
@@ -32,3 +32,15 @@ class AbrirMinhaAgendaUI:
           st.success("Realizado com sucesso")
           time.sleep(2)
           st.rerun()
+        def alterar_senha():
+          proficionais = View.cliente_listar()
+          if len(proficionais) == 0: st.write("Nenhuma senha cadastrada")
+          else:
+            op = st.selectbox("Atualização de Proficionais", proficionais)
+            senha = st.text_input("Nova senha", op.get_senha(), type="password")
+            if st.button("Alterar_Senha"):
+              id = op.get_id()
+              View.cliente_atualizar(id, senha)
+              st.success("Senha alterada com sucesso")
+              time.sleep(2)
+              st.rerun()
