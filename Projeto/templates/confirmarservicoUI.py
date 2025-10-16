dea1ef3a18e544c1d744d0bb8e554c8681d086de
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-16 12:22:56 +0000

    mensagem

--- Projeto/templates/confirmarservicoUI.py
+++ Projeto/templates/confirmarservicoUI.py
@@ -13,3 +13,5 @@ class ConfrirmarServicoUI:
               st.success("Conta criada com sucesso")
               time.sleep(2)
               st.rerun()
+          templates.abrirminhaagendaUI(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_cliente, id_servico, id_proficional)
+          st.success("Horário inserido com sucesso")
