4cf1bb441e8ec239da7d9df8347f175812f122b8
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-16 12:39:19 +0000

    mensagem

--- Projeto/templates/confirmarservicoUI.py
+++ Projeto/templates/confirmarservicoUI.py
@@ -1,17 +1,16 @@
 import streamlit as st
-from templates import AbrirMinhaAgendaUI
+from views import View
 import time
 import datetime
-
-class ConfrirmarServicoUI:
+class ConfirmarServicoUI:
       def confirmar():
           st.header("Abrir Conta no Sistema")
           horario = st.strptime_input("Informe o horario")
           cliente = st.text_input("Informe o cliente", type="password")
           if st.button("Inserir"):
-              templates.abrirminhaagendaUI(horario, cliente)
+              View.abrirminhaagendaUI(horario, cliente)
               st.success("Conta criada com sucesso")
               time.sleep(2)
               st.rerun()
-          templates.abrirminhaagendaUI(datetime.strptime(horario, "%d/%m/%Y %H:%M"), horario)
+          View.abrirminhaagendaUI(datetime.strptime(horario, "%d/%m/%Y %H:%M"), horario)
           st.success("Horário inserido com sucesso")
