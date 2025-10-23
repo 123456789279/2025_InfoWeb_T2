7719f0059189cce0ebc1bb2de87620fb593f8d6f
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-23 13:18:00 +0000

    mensagem

--- Projeto/models/horario.py
+++ Projeto/models/horario.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 class Horario:
    def __init__(self, id, data):
-     self.set_id(id)
-     self.set_data(data)
-     self.set_confirmado(False)
-     self.set_id_cliente(0)
-     self.set_id_servico(0)
-     self.set_id_proficional(0)
+     self.__set_id = id
+     self.__set_data = data
+     self.__set_confirmado = False
+     self.__set_id_cliente = 0
+     self.__set_id_servico = 0
+     self.__set_id_proficional =- 0
    def __str__(self):
      return f"{self.__id} - {self.__data.strftime('%d/%m/%Y %H:%M')} - {self.__confirmado}"
    def get_id(self): 
@@ -24,6 +24,7 @@ class Horario:
    def set_id(self, id): 
      self.__id = id
    def set_data(self, data): 
+     if data < 0: raise ValueError("data inválida") 
      self.__data = data
    def set_confirmado(self, confirmado): 
      self.__confirmado = confirmado
