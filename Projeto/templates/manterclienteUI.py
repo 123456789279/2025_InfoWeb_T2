da681ae6161727f36e4d7d12b047ed2b1acda8e5
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-23 12:36:39 +0000

    mensagem

--- Projeto/templates/manterclienteUI.py
+++ Projeto/templates/manterclienteUI.py
@@ -25,6 +25,11 @@ class ManterClienteUI:
     fone = st.text_input("Informe o fone")
     senha = st.text_input("Informe a senha", type="password")
     if st.button("Inserir"):
+      try:
+          View.cliente_inserir(email, float(fone))
+          st.success("Cliente inserido com sucesso")
+      except ValueError as erro:
+          st.error(erro)
       View.cliente_inserir(nome, email, fone, senha)
       st.success("Cliente inserido com sucesso")
       time.sleep(2)
