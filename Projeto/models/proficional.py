934db8237a9ad92a9945b9d44cff9dbb99123a44
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-23 12:54:59 +0000

    mensagem

--- Projeto/models/proficional.py
+++ Projeto/models/proficional.py
@@ -1,11 +1,11 @@
 class Proficional:
     def __init__(self, id, nome, especialidade, conselho, email, senha):
-        self.__id = id
-        self.__nome = nome
-        self.__especialidade = especialidade
-        self.__conselho = conselho
-        self.__email = email
-        self.__senha = senha
+        self.__id = 0
+        self.__nome = ""
+        self.__especialidade = ""
+        self.__conselho = ""
+        self.__email = ""
+        self.__senha = 0
     def __str__(self):
         return f"{self.__id}-{self.__nome}-{self.__especialidade}-{self.__conselho}"
     def get_id(self):
