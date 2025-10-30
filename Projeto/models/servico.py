08780349a78f7981da907640a180e576899abb8c
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-10-30 13:12:31 +0000

    mensagem

--- Projeto/models/servico.py
+++ Projeto/models/servico.py
@@ -38,12 +38,12 @@ class ServicoDAO(DAO):
 
     @classmethod
     def abrir(cls):
-      cls.__objetos = []
-      try:
-        with open("servicos.json", mode="r") as arquivo:
-          list_dic = json.load(arquivo)
-          for dic in list_dic:
-            obj = Servico.from_json(dic)
-            cls.__objetos.append(obj)
-      except FileNotFoundError:
-        pass
+        cls.__objetos = []
+        try:
+            with open("servicos.json", mode="r") as arquivo:
+                list_dic = json.load(arquivo)
+                for dic in list_dic:
+                    obj = Servico.from_json(dic)
+                    cls.__objetos.append(obj)
+        except FileNotFoundError:
+            pass
