1d8eba7958fe9f1b16c542bc9c8795eeb11091af
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-12-16 14:04:31 +0000

    mensagem

--- SegundoProjeto/templates/manteragendamentoUI.py
+++ SegundoProjeto/templates/manteragendamentoUI.py
@@ -19,31 +19,20 @@ class ManterAgendamentoUI:
        dic = []
        for obj in agendamentos:
          funcionario = View.funcionario_listar_id(obj.get_id_funcionario())
-         servico = View.servico_listar_id(obj.get_id_servico())
-         proficional = View.proficional_listar_id(obj.get_id_proficional())
          if funcionario != None: funcionario = funcionario.get_nome()
-         if servico != None: servico = servico.get_descricao()
-         dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "funcionario" : funcionario, "serviço" : servico, "proficional" : proficional})
+         dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "funcionario" : funcionario})
        df = pd.DataFrame(dic)
        st.dataframe(df)
    def inserir():
      funcionarios = View.funcionario_listar()
-     servicos = View.servico_listar()
-     proficional = View.proficional_listar()
      data = st.text_input("Informe a data e agendamento do serviço", datetime.now().strftime("%d/%m/%Y %H:%M"))
      confirmado = st.checkbox("Confirmado")
      funcionario = st.selectbox("Informe o funcionario", funcionarios, index = None)
-     servico = st.selectbox("Informe o serviço", servicos, index = None)
-     proficional = st.selectbox("Informe o proficional", proficional, index = None)
      if st.button("Inserir"):
       if funcionario != None: 
          id_funcionario= funcionario.get_id()
-      if servico != None: 
-         id_servico = servico.get_id()
-      if proficional != None: 
-         id_proficional = proficional.get_id()
       try:
-          View.agendamento_inserir(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_funcionario, id_servico, id_proficional)
+          View.agendamento_inserir(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_funcionario)
           st.success("Agendamento inserido com sucesso")
       except ValueError as erro:
           st.error(erro)
@@ -54,18 +43,12 @@ class ManterAgendamentoUI:
      if len(agendamentos) == 0: st.write("Nenhum agendamento cadastrado")
      else:
        funcionarios = View.funcionario_listar()
-       servicos = View.servico_listar()
-       proficionais = View.proficional_listar()
        op = st.selectbox("Atualização de Horários", agendamentos)
        data = st.text_input("Informe a nova data e horário do serviço", op.get_data().strftime("%d/%m/%Y %H:%M"))
        confirmado = st.checkbox("Nova confirmação", op.get_confirmado())
        id_funcionario = None if op.get_id_funcionario() in [0, None] else op.get_id_funcionario()
-       id_servico = None if op.get_id_servico() in [0, None] else op.get_id_servico()
-       id_proficional = None if op.get_id_proficional() in [0, None] else op.get_id_proficional()
        #id_serviço ...
        funcionario = st.selectbox("Informe o novo funcionario", funcionarios, next((i for i, c in enumerate(funcionarios) if c.get_id() == id_funcionario), None))
-       servico = st.selectbox("Informe o novo serviço", servicos, next((i for i, s in enumerate(servicos) if s.get_id() == id_servico), None))
-       proficional = st.selectbox("Informe o novo proficional", proficionais, next((i for i, s in enumerate(proficionais) if s.get_id() == id_proficional), None))
        if st.button("Atualizar"):
         try:
             id = op.get_id()
@@ -74,11 +57,7 @@ class ManterAgendamentoUI:
         except ValueError as erro:
             st.error(erro)
         if funcionario != None: id_funcionario = funcionario.get_id()
-        if servico != None: id_servico = servico.get_id()
-        if proficional != None: id_proficional = proficional.get_id()
         id_funcionario = None
-        id_servico = None
-        id_proficional = None
         time.sleep(2)
         st.rerun()
    def excluir():
