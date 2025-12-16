c7c740bbac60ad27e2907f5a9ee9e30e294704ed
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2025-12-16 14:01:42 +0000

    mensagem

--- SegundoProjeto/templates/manteragendamentoUI.py
+++ SegundoProjeto/templates/manteragendamentoUI.py
@@ -18,71 +18,71 @@ class ManterAgendamentoUI:
      else:
        dic = []
        for obj in agendamentos:
-         cliente = View.cliente_listar_id(obj.get_id_cliente())
+         funcionario = View.funcionario_listar_id(obj.get_id_funcionario())
          servico = View.servico_listar_id(obj.get_id_servico())
          proficional = View.proficional_listar_id(obj.get_id_proficional())
-         if cliente != None: cliente = cliente.get_nome()
+         if funcionario != None: funcionario = funcionario.get_nome()
          if servico != None: servico = servico.get_descricao()
-         dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "cliente" : cliente, "serviço" : servico, "proficional" : proficional})
+         dic.append({"id" : obj.get_id(), "data" : obj.get_data(), "confirmado" : obj.get_confirmado(), "funcionario" : funcionario, "serviço" : servico, "proficional" : proficional})
        df = pd.DataFrame(dic)
        st.dataframe(df)
    def inserir():
-     clientes = View.cliente_listar()
+     funcionarios = View.funcionario_listar()
      servicos = View.servico_listar()
      proficional = View.proficional_listar()
      data = st.text_input("Informe a data e agendamento do serviço", datetime.now().strftime("%d/%m/%Y %H:%M"))
      confirmado = st.checkbox("Confirmado")
-     cliente = st.selectbox("Informe o cliente", clientes, index = None)
+     funcionario = st.selectbox("Informe o funcionario", funcionarios, index = None)
      servico = st.selectbox("Informe o serviço", servicos, index = None)
      proficional = st.selectbox("Informe o proficional", proficional, index = None)
      if st.button("Inserir"):
-      if cliente != None: 
-         id_cliente= cliente.get_id()
+      if funcionario != None: 
+         id_funcionario= funcionario.get_id()
       if servico != None: 
          id_servico = servico.get_id()
       if proficional != None: 
          id_proficional = proficional.get_id()
       try:
-          View.agendamento_inserir(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_cliente, id_servico, id_proficional)
+          View.agendamento_inserir(datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_funcionario, id_servico, id_proficional)
           st.success("Agendamento inserido com sucesso")
       except ValueError as erro:
           st.error(erro)
       time.sleep(2)
       st.rerun()
    def atualizar():
-     agendamentos = View.horario_listar()
+     agendamentos = View.agendamento_listar()
      if len(agendamentos) == 0: st.write("Nenhum agendamento cadastrado")
      else:
-       clientes = View.cliente_listar()
+       funcionarios = View.funcionario_listar()
        servicos = View.servico_listar()
        proficionais = View.proficional_listar()
        op = st.selectbox("Atualização de Horários", agendamentos)
        data = st.text_input("Informe a nova data e horário do serviço", op.get_data().strftime("%d/%m/%Y %H:%M"))
        confirmado = st.checkbox("Nova confirmação", op.get_confirmado())
-       id_cliente = None if op.get_id_cliente() in [0, None] else op.get_id_cliente()
+       id_funcionario = None if op.get_id_funcionario() in [0, None] else op.get_id_funcionario()
        id_servico = None if op.get_id_servico() in [0, None] else op.get_id_servico()
        id_proficional = None if op.get_id_proficional() in [0, None] else op.get_id_proficional()
        #id_serviço ...
-       cliente = st.selectbox("Informe o novo cliente", clientes, next((i for i, c in enumerate(clientes) if c.get_id() == id_cliente), None))
+       funcionario = st.selectbox("Informe o novo funcionario", funcionarios, next((i for i, c in enumerate(funcionarios) if c.get_id() == id_funcionario), None))
        servico = st.selectbox("Informe o novo serviço", servicos, next((i for i, s in enumerate(servicos) if s.get_id() == id_servico), None))
        proficional = st.selectbox("Informe o novo proficional", proficionais, next((i for i, s in enumerate(proficionais) if s.get_id() == id_proficional), None))
        if st.button("Atualizar"):
         try:
             id = op.get_id()
-            View.horario_atualizar(op.get_id(), datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_cliente, id_servico, id_proficional)
+            View.horario_atualizar(op.get_id(), datetime.strptime(data, "%d/%m/%Y %H:%M"), confirmado, id_funcionario, id_servico, id_proficional)
             st.success("Horário atualizado com sucesso")
         except ValueError as erro:
             st.error(erro)
-        if cliente != None: id_cliente = cliente.get_id()
+        if funcionario != None: id_funcionario = funcionario.get_id()
         if servico != None: id_servico = servico.get_id()
         if proficional != None: id_proficional = proficional.get_id()
-        id_cliente = None
+        id_funcionario = None
         id_servico = None
         id_proficional = None
         time.sleep(2)
         st.rerun()
    def excluir():
-     agendamentos = View.horario_listar()
+     agendamentos = View.agendamento_listar()
      if len(agendamentos) == 0: st.write("Nenhum horário cadastrado")
      else:
        op = st.selectbox("Exclusão de Horários", agendamentos)
