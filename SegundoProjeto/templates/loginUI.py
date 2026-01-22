9eeb65f4596246f2961d665e6199dbffb0b9efee
123456789279 <ranielly.brito@escolar.ifrn.edu.br>
2026-01-22 12:38:15 +0000

    mensagem

---
+++ SegundoProjeto/templates/loginUI.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from views import View
+
+class LoginUI:
+    def funcionario_autenticar(email, senha):
+        for c in View.funcionario_listar():
+           if c.get_email() == email and c.get_senha() == senha:
+              return {"id": c.get_id(), "nome": c.get_nome()}
+        return None
+    def main():
+        st.header("Entrar no Sistema")
+        email = st.text_input("Informe o e-mail")
+        senha = st.text_input("Informe a senha", type="password")
+        if st.button("Entrar"):
+            c = View.funcionario_autenticar(email, senha)
+            if c == None: st.write("E-mail ou senha inválidos")
+            else:
+                st.session_state["usuario_id"] = c["id"]
+                st.session_state["usuario_nome"] = c["nome"]
+                st.rerun()
